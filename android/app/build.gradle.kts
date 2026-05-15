52c5daaa8023d0009fc0b765f308c76eb964efd4
lvqx <lvqixing@ddpai.com>
2026-05-15 11:40:05 +0800

    release: 更改包名为 com.hoshi.ledger, 重置版本为 1.0.0, 更新 README

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.hoshi_ledger"
+    namespace = "com.hoshi.ledger"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.hoshi_ledger"
+        applicationId = "com.hoshi.ledger"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/hoshi/ledger/MainActivity.kt
+++ android/app/src/main/kotlin/com/hoshi/ledger/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.hoshi_ledger
+package com.hoshi.ledger
 
 import io.flutter.embedding.android.FlutterActivity
 
